a044c735a47bad3d077db7fb22a0a0cd0bdd5c79
GMerencio <gmerencio.santos@gmail.com>
2023-05-05 00:23:40 -0300

    Modelo de chuva gerado pelo Prophet e adicionado à visualização

--- streamlit-app.py
+++ streamlit-app.py
@@ -11,11 +11,20 @@ def carregar_modeloVolProphet():
 	with open('modelo-prophet/modelo-volume.json', 'r') as file:
 		return model_from_json(file.read())
 
+@st.cache_data
+def carregar_modeloChuvaProphet():
+	with open('modelo-prophet/modelo-chuva.json', 'r') as file:
+		return model_from_json(file.read())
+
 # Carregar dados previstos
 
 @st.cache_data
 def carregar_previsaoVolProphet():
 	return pd.read_pickle('modelo-prophet/previsao-volume.pkl')
+
+@st.cache_data
+def carregar_previsaoChuvaProphet():
+	return pd.read_pickle('modelo-prophet/previsao-chuva.pkl')
 	
 # Interface
 
@@ -23,6 +32,12 @@ st.title('Previsão de volume do Sistema Cantareira')
 
 modeloVolProphet = carregar_modeloVolProphet()
 previsaoVolProphet = carregar_previsaoVolProphet()
+modeloChuvaProphet = carregar_modeloChuvaProphet()
+previsaoChuvaProphet = carregar_previsaoChuvaProphet()
 
+st.header('Volume (em hm³)')
 st.plotly_chart(plot_plotly(modeloVolProphet, previsaoVolProphet))
-st.write(previsaoVolProphet[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
+#st.write(previsaoVolProphet[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
+
+st.header('Chuva (em mm)')
+st.plotly_chart(plot_plotly(modeloChuvaProphet, previsaoChuvaProphet))
